80426981187c2f6e999eac32ac75f905820e30bb
Dima <dima@zaharov.info>
2016-03-30 21:45:49 +0300

    No time diff, if the timezone was  adjusted for OpenShift cartridge

--- src/main/java/lv/llu/bites/utils/UrlDateAsLongParam.java
+++ src/main/java/lv/llu/bites/utils/UrlDateAsLongParam.java
@@ -11,13 +11,18 @@ public class UrlDateAsLongParam {
     /**
      * Time Difference between OpenShift's US/Eastern Time and GMT+2 of Latvia
      */
-    public static final int PLUS_6_HOURS_FOR_LATVIA = 21600000;
+    public static final int TIME_DIFF_IN_MSEC_FOR_LATVIA = 0; // or 21600000;
+    /**
+     * No difference, if the timezone was adjusted for the particular OpenShit cartridge like that:
+     * rhc env-set JAVA_OPTS_EXT=" -Duser.timezone=Europe/Riga " --app %APPNAME%
+     */
+
     private Date date;
 
     public UrlDateAsLongParam(String in) throws WebApplicationException {
         try {
             final Long incomingMilliseconds = Long.valueOf(in);
-            date = new Date(incomingMilliseconds + PLUS_6_HOURS_FOR_LATVIA);
+            date = new Date(incomingMilliseconds + TIME_DIFF_IN_MSEC_FOR_LATVIA);
         } catch (Exception exception) {
             throw new WebApplicationException(400);
         }
